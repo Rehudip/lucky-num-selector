4a5f8a85cb5fa5633555b1faa9c0c55d0d50721b
rehudip <racingcup3@gmail.com>
2023-05-01 14:28:47 +0900

    EDIT: selector.py
    make selector.py to loop by the condition

--- selector.py
+++ selector.py
@@ -3,8 +3,14 @@ import random
 def select_6():
 	numbers = []
 	for i in range(6):
-		numbers.append(random.randint(1,46))
+		numbers.append(random.randint(1,45))
 	print(numbers)
 
+print("printing 6 numbers. press 'q' to quit.")
 
-select_6()
+while True:
+	if (input() == 'q'):
+		print('quitting...')
+		break
+	else:
+		select_6()
